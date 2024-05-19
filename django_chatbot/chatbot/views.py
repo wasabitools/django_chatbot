fc7ff8036772e21064d3baba90cf585c61489633
natasafi <natasafrag@gmail.com>
2024-05-19 16:46:38 +0100

    introduces ai

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,11 +1,29 @@
+from django.conf import settings
+import openai
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+client = openai.OpenAI(
+    api_key=settings.OPENAI_API_KEY
+)
+
+
+def question_openai(message):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo", 
+        messages=[{"role": "user", "content": message}],
+        stop=None,
+        n=1,
+        temperature=0.1
+    )
+    answer = response.choices[0].message.content
+    return answer
+
 
 def chatbot(request):
-    if request.method == 'POST':
-        message = request.POST.get('message')
-        response = "Oh Gawd!"
-        return JsonResponse({'message': message, 'response': response})
+    if request.method == "POST":
+        message = request.POST.get("message")
+        response = question_openai(message)
+        return JsonResponse({"message": message, "response": response})
     return render(request, "chatbot.html")
--- django_chatbot/django_chatbot/settings.py
+++ django_chatbot/django_chatbot/settings.py
@@ -11,9 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Read the .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Now you can access the environment variables
+OPENAI_API_KEY = env('OPENAI_API_KEY')
 
 
 # Quick-start development settings - unsuitable for production
