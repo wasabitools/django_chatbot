07df44df91b5d4808c0ef75adc7e2e6288911fc1
natasafi <natasafrag@gmail.com>
2024-05-19 15:05:02 +0100

    adds script to handle user message

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,6 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = "I'm fucking talking to you."
+        return JsonResponse({'message': message, 'response': response})
     return render(request, "chatbot.html")
